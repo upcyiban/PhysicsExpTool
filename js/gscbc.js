ad2089d547393de7da09c1941aa053ace489f96f
77dfeba <349841726@qq.com>
2015-11-19 23:02:15 +0800

    光栅测量波长 完成

--- js/gscbc.js
+++ js/gscbc.js
@@ -8,11 +8,13 @@ $(document).ready(function(){
         var a21 = $('#angle3').val();
         var a22 = $('#angle4').val();
         var k = $('#k').val();
-        var c = 0.25*(a11 - a21 + a12 - a22);
-        var z = parseInt("12.2");
+        var c = 0.25*(Math.abs(a11 - a21) + Math.abs(a12 - a22));
+        var z = parseInt(c);
+        var x = (c - z)/60;
         var p = Math.PI;
-/*        var a;
-        var l = 0.01*(Math.sin(a))/k;*/
-        $("#record").append("<tr> <th>"+a11+"</th> <th>"+a12+"</th> <th>"+a21+"</th> <th>"+a22+"</th> <th>"+z+"</th> </tr>");
+        c = (x + z)*p/180;
+        var l = 0.01*(Math.sin(c))/k*Math.pow(10,9);
+        l = l.toFixed(4);
+        $("#record").append("<tr> <th>"+a11+"</th> <th>"+a12+"</th> <th>"+a21+"</th> <th>"+a22+"</th> <th>"+l+"</th> </tr>");
     });
 });
