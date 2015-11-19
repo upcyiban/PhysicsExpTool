2c797271b9b38a313c040f3c886625d39317110f
sherhow <84416281@qq.com>
2015-11-19 23:14:16 +0800

    添加新的伏安特性页面

---
+++ toolspage/fatx.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no;">
+    <title>电学元件的伏安特性研究</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/main.css" rel="stylesheet">
+    <script src="../js/jquery-2.1.4.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/fatx.js"></script>
+</head>
+<body>
+
+<div id="container" class="container">
+    <h1><strong>电学元件的伏安特性研究</strong></h1>
+    <p>研究二极管正向电压和电流关系</p>
+    <br>
+    <div class="panel panel-default">
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <br>
+            <div class="form-horizontal">
+                <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-2 control-label">U1(单位:V)</label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="m1" placeholder="输入U1的值">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-2 control-label">I1(单位:mA)</label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="m2" placeholder="输入I1的值">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-2 control-label">U2(单位:V)</label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="v1" placeholder="输入U2的值">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword3" class="col-sm-2 control-label">I2(单位:mA)</label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="v2" placeholder="输入I2的值">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-12">
+                        <button id="dlshbtn" class="btn btn-default">计算</button>
+                    </div>
+                </div>
+            </div>
+            <h2>历史记录</h2>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>U1</th>
+                    <th>I1</th>
+                    <th>U2</th>
+                    <th>I2</th>
+                    <th>A</th>
+                    <th>B</th>
+                </tr>
+                </thead>
+                <tbody id="record">
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <footer class="text-center">
+        <p>Powered by upcyiban</p>
+    </footer>
+</div>
+</body>
+</html>
