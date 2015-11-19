28fc0fe8df5aa996664ad56ad098d63d987e7eba
skyADMIN <546485299@qq.com>
2015-11-19 17:49:31 +0800

    反馈、留言页面

---
+++ question.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no;">
+    <title>PhysicsExpTool</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+<body>
+<div id="container" class="container-fluid">
+    <h1>问题反馈及留言建议</h1>
+    <div class="width-ctrl">
+        <div id="warm" class="warm"></div>
+        <div class="panel">
+            <div class="panel-body">
+                123
+            </div>
+        </div>
+    </div>
+    <footer class="text-center">
+        <p>Powered by upcyiban</p>
+    </footer>
+</div>
+</body>
+</html>
