ae8286b7797c44e65316383dd14a1a742ec8def6
skyADMIN <546485299@qq.com>
2015-11-26 19:21:27 +0800

    modify header mata

--- toolspage/dlsh.php
+++ toolspage/dlsh.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no;">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <title>动量守恒定律的验证</title>
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="../css/main.css" rel="stylesheet">
