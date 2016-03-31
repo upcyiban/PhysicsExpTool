d220a832a2f3b7024ba13062137e0b4eae44a81d
wanghaojun <549180928@qq.com>
2016-03-31 16:43:31 +0800

    一点小改动

--- js/xxzl.js
+++ js/xxzl.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
         var p = $('#p').val();//p
         var a=u*i/(0.52*p);
         a= a.toFixed(4);
-        $("#record").append("<tr>  <td style='word-break: break-all'>" + u + "<br>" + i + "<br> " + p + "<br> " + a+ "</td>"+
+        $("#record").append("<tr>  <td style='word-break: break-all'>" + u + "<br>" + i + "<br> " + p +  "</td>"+
             "<td style='word-break: break-all'>" + u + "</td> " +
             " <td style='word-break: break-all'>" + i  + "</td>" +
             " <td style='word-break: break-all'>" + p  + "</td>" +
